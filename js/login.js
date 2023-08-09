53d049b1becaecc0566c30f34f0e355a989d4e5c
facudacosta1 <facurivero29@gmail.com>
2023-08-09 18:42:55 -0300

    Subo login

---
+++ js/login.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var userInput = document.getElementById('user');
+    var passwordInput = document.getElementById('password');
+    var loginButton = document.getElementById('login-button');
+
+    loginButton.addEventListener('click', function(event) {
+        if (userInput.value.length === 0 || passwordInput.value.length === 0) {
+            event.preventDefault(); // Detener la redirección si los campos no están llenos
+        }
+    });
+
+    function actualizarEstilos() {
+        if (userInput.value.length > 0 && passwordInput.value.length > 0) {
+            loginButton.classList.add('hovered');
+        } else {
+            loginButton.classList.remove('hovered');
+        }
+    }
+
+    userInput.addEventListener('input', actualizarEstilos);
+    passwordInput.addEventListener('input', actualizarEstilos);
+});
