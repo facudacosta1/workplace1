558366431c30a579e9c311069f434ec90bf3f383
romanmonfort <137888802+romanmonfort@users.noreply.github.com>
2023-09-01 08:47:07 -0300

    mostrar el usuario
    /

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+
+    var storedUser = localStorage.getItem('user');
+    var storedPassword = localStorage.getItem('password');
+
+    if (!storedUser || !storedPassword){
+        window.location = 'login.html'
+    }
+
+    let nameUser = document.getElementById('nameUser');
+    nameUser.innerText = storedUser;
+    
+});
