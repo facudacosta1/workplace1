d392e0e0cb9facc7d2e58e1ae89aa0eb3eb5fc9c
facudacosta1 <facurivero29@gmail.com>
2023-09-10 15:59:10 -0300

    actualizo

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,8 @@
 const BASE_URL = 'https://japceibal.github.io/emercado-api/cats_products/'; 
 const catID = localStorage.getItem('catID'); 
 const productID=localStorage.getItem('productID');
-const productInfoContainer=document.getElementById('product-info-container')
+const productInfoContainer=document.getElementById('product-info-container');
+const comentsContainer = document.getElementById('coments-container')
 let jsonData;
 
 async function loadAndDisplayData(){
@@ -38,10 +39,46 @@ function displayProducts(product){
             <p class="product-cost">$USD ${product.cost} </p>
             <p>${product.description}</p>
             <p>Categoría:  ${jsonData.catName}</p>
-            <p>Cantidad de artículos vendidos: ${product.soldCount}</p>
+            <p>${product.soldCount} artículos vendidos</p>
         </div>
         
     `
     productInfoContainer.innerHTML=content;
 
+}
+
+//FUNCION PARA OBTENER JSON DE COMENTARIOS
+
+async function getComments(){
+    const url = PRODUCT_INFO_COMMENTS_URL + productID + '.json'
+    try {
+        const response = await fetch (url);
+        if(!response.ok){
+            throw new Error('Error fetching data')
+        }
+        comentsData = await response.json();
+        displayComents(comentsData);
+    } catch (error) {
+        
+    }
+}
+getComments();
+
+//FUNCION PARA MOSTRAR COMENTARIOS
+
+function displayComents(coments) {
+        const comentarios = document.createElement('h1');
+        comentarios.textContent='Comentarios'
+        comentsContainer.appendChild(comentarios)
+        
+        coments.forEach(coment => {
+            const p = document.createElement('p');
+            p.classList.add('coment');
+            let stars = '';
+            for (let i = 0; i < coment.score; i++) {
+                stars += '⭐';
+            }
+            p.textContent = `${stars} ${coment.user}: ${coment.description}`;
+            comentsContainer.appendChild(p);
+        });
 }
