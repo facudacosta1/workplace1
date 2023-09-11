99f2b521e26d6ffbe315c95682ab01bc7d26ab81
gastonmieres <g4ston.m1eres@gmail.com>
2023-09-11 18:46:19 -0300

    Cambios de estilo en product-info.html
    Me pidió hacer un merge, me arrepentí y lo he deshecho. De pasada cambie el estilo a la parte de poner un comentario y estrellas.

--- js/product-info.js
+++ js/product-info.js
@@ -2,7 +2,8 @@ const BASE_URL = 'https://japceibal.github.io/emercado-api/cats_products/';
 const catID = localStorage.getItem('catID'); 
 const productID=localStorage.getItem('productID');
 const productInfoContainer=document.getElementById('product-info-container');
-const comentsContainer = document.getElementById('coments-container')
+const comentsContainer = document.getElementById('coments-container');
+var storedUser = localStorage.getItem('user');
 let jsonData;
 
 async function loadAndDisplayData(){
@@ -67,18 +68,71 @@ getComments();
 //FUNCION PARA MOSTRAR COMENTARIOS
 
 function displayComents(coments) {
-        const comentarios = document.createElement('h1');
-        comentarios.textContent='Comentarios'
-        comentsContainer.appendChild(comentarios)
-        
-        coments.forEach(coment => {
-            const p = document.createElement('p');
-            p.classList.add('coment');
-            let stars = '';
-            for (let i = 0; i < coment.score; i++) {
-                stars += '⭐';
-            }
-            p.textContent = `${stars} ${coment.user}: ${coment.description}`;
-            comentsContainer.appendChild(p);
-        });
+    const comentHeader = document.createElement('h1');
+    comentHeader.innerText = 'Comentarios'
+    comentsContainer.appendChild(comentHeader);
+  
+    coments.forEach(comment => {
+      const p = document.createElement('p');
+      p.classList.add('comment');
+  
+      // Agrega las estrellas al comentario
+      let stars = '';
+      for (let i = 0; i < comment.score; i++) {
+        stars += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="yellow" stroke="black" stroke-width="1" style="margin-right: 2px;" class="bi bi-star-fill" viewBox="0 0 16 16">
+          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+        </svg>`;
+      }
+  
+      p.innerHTML = `${stars} ${comment.user}: ${comment.description}`;
+      comentsContainer.appendChild(p);
+    });
+  }
+
+function postComentario(){
+    let datos = {
+        "product": productID,
+        "score": calificacion.value,
+        "user": storedUser,
+    }
+
+    const url = 'https://japceibal.github.io/emercado-api/products_comments/' + productID + '.json';
+
+    fetch(url, {
+        method:"POST",
+        body: JSON.stringify(datos),
+        headers:{"Content-type":"application/json; charset=UTF-8"}
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud de red.');
+        }
+        return response.json();
+    })
+    .then(json=>console.log(json))
+    .catch(err=>console.log(err))
 }
+
+function enviarComentario(){
+    const comentarioInput = document.getElementById('comentarioInput');
+    const comentario = document.createElement('p');
+    const calificacion = document.getElementById('calificacion')
+    comentario.classList.add('comment');
+
+    let stars = '';
+    for (let i = 0; i < calificacion.value; i++) {
+        stars += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="yellow" stroke="black" stroke-width="1" style="margin-right: 2px;" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+        </svg>`;
+    }
+
+    comentario.innerHTML = `${stars} ${storedUser}: ${comentarioInput.value}`;
+    comentsContainer.appendChild(comentario);
+}
+
+const enviarBtn = document.getElementById('enviarBtn');
+
+enviarBtn.addEventListener('click', function(e){
+    enviarComentario();
+    postComentario();
+});
