cf28fe0a89aa551244a9cbb36cec50f8ab11dfb5
gastonmieres <g4ston.m1eres@gmail.com>
2023-09-30 19:06:38 -0300

    Cambio en el estilo de las estrellas
    Cambie el estilo de las estrellas de la calificación de los productos, ahora tienen una estructura de 5 estrellas, y si por ejemplo la calificación es de 3 estrellas se muestran 3 estrellas de color amarillo y las otras 2 estrellas de color negro.

--- js/product-info.js
+++ js/product-info.js
@@ -100,11 +100,19 @@ function showComments() {
     comentsData.forEach(coment => {
         // Crear un string de estrellas en base a coment.score
         let stars = '';
-        for (let i = 0; i < coment.score; i++) {
-            stars += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="yellow" stroke="black" stroke-width="1" style="margin-right: 2px;" class="bi bi-star-fill" viewBox="0 0 16 16">
+        for (let i = 0; i < 5; i++) {
+          if (i < coment.score) {
+            // Estrella llena (amarilla) para puntuaciones alcanzadas
+            stars += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFC400" stroke="black" stroke-width="1" style="margin-right: 2px;" class="bi bi-star-fill" viewBox="0 0 16 16">
             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-          </svg>`; // Puedes usar cualquier carácter o icono para representar una estrella
+        </svg>`;
+        } else {
+            // Estrella vacía (negra) para puntuaciones no alcanzadas
+            stars += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#212529" stroke="black" stroke-width="1" style="margin-right: 2px;" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+        </svg>`;
         }
+    }
 
         content += `
             <p>${stars} ${coment.user}: ${coment.description}</p>
